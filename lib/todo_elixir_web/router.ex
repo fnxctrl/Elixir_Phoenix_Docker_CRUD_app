966634ecb0c2fe7edeccca28fd10f2e1d7495efa
fnxctrl <43869304+fnxctrl@users.noreply.github.com>
2019-05-26 17:31:04 +1200

    Add files via upload

---
+++ lib/todo_elixir_web/router.ex
@@ -0,0 +1,28 @@
+defmodule TodoElixirWeb.Router do
+  use TodoElixirWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TodoElixirWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+
+    resources "/todos", TodoController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", TodoElixirWeb do
+  #   pipe_through :api
+  # end
+end
